d644d8bf18c7e3862552d55a0f0080252a95d5b2
Aleksandr Koltovich <avkoltovich@gmail.com>
2019-10-08 12:55:25 +0300

    Добавил интерактивную карту

--- js/script.js
+++ js/script.js
@@ -41,3 +41,37 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+// Yandex карта
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [59.938631, 30.323055],
+          zoom: 17
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Gllacy Shop',
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: '../img/pointer.svg',
+          // Размеры метки.
+          iconImageSize: [80, 140],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-40, -140]
+      });
+
+  myMap.geoObjects
+      .add(myPlacemark);
+});
